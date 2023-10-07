27df346de5c23bb619af960ceea00ecaff553a91
kipngenohaaron <kipngenohaaron@gmail.com>
2023-10-07 20:18:48 +0300

    events done

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -13,28 +13,28 @@ const Events = () => {
   const [events, setEvents] = useState([
     {
       id: 1,
-      title: 'Event 1',
+      title: 'Burudani Palace',
       description:
-        'Join us for Event 1, where we come together to make a positive impact on the lives of needy children. Your support in the form of funds, food, and more is greatly appreciated!',
+        'Join us for Event at Burudani Palace, where we come together to make a positive impact on the lives of needy children. Your support in the form of funds, food, and more is greatly appreciated!',
       date: '2023-10-10',
-      location: 'Location 1',
+      location: 'Nairobi, Kenya',
     },
     {
       id: 2,
-      title: 'Event 2',
+      title: 'Quivers of the World!',
       description:
-        'Event 2 is a wonderful opportunity to make a difference. Let\'s unite to provide hope and nourishment to those in need. Your contributions matter!',
+        'This is a wonderful opportunity to make a difference. Let\'s unite to provide hope and nourishment to those in need. Your contributions matter!',
       date: '2023-10-15',
-      location: 'Location 2',
+      location: 'Ngong Road, Nairobi',
      
     },
     {
       id: 3,
-      title: 'Event 3',
+      title: 'The Annex',
       description:
-        'At Event 3, we focus on empowering the future generation. Together, we can ensure a brighter tomorrow for underprivileged children. Join us on this mission!',
+        'At The Annex, we focus on empowering the future generation. Together, we can ensure a brighter tomorrow for underprivileged children. Join us on this mission!',
       date: '2023-10-20',
-      location: 'Location 3',
+      location: 'Eldoret, Uasin Gishu',
     
     },
   ]);
